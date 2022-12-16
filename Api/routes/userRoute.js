1d27868fe2d40c36580345416c74ca8798975f74
chau_c <chau_c@etna-alternance.net>
2022-12-16 16:43:04 +0100

    fix: userRoute delete endpoint

--- Api/routes/userRoute.js
+++ Api/routes/userRoute.js
@@ -13,6 +13,6 @@ userRouter.post("/register", register);
 userRouter.post("/login", login);
 userRouter.get("/users/:id", getOneUser);
 userRouter.get("/users", getAllUsers);
-userRouter.delete("/users/:id", deleteUser);
+userRouter.delete("/user/:id", deleteUser);
 
 module.exports = userRouter;

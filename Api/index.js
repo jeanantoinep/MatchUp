9ca23203847ce427a8c5064b9ea8e7c013bca6b1
MaloLelandais <leland_m@etna-alternance.net>
2022-12-16 10:24:46 +0100

    add index

--- Api/index.js
+++ Api/index.js
@@ -1,12 +1,34 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const user = require("./routes/routesUser");
+const product = require("./routes/routeProduct");
+const order = require("./routes/orderRoute");
 
-const port = 5000;
-app.listen(port, () => {
-  console.log('Listening on port ' + port);
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.listen(3000, () => console.log("Listening on port 3000"));
+
+require("dotenv").config();
+
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
 });
 
-app.get('/', (req, res) => res.send('My first REST API!'));
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+app.use("/api", user);
+app.use("/api", product);
+app.use("/api", order);
+app.get("/", (req, res) => res.send("My first REST API!"));
 app.listen(port, () => {
-  console.log('Listening on port ' + port);
+  console.log("Listening on port " + port);
 });

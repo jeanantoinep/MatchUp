df098aba1be7a0abdbbc18d9a71bdfa2e4b779f3
MaloLelandais <leland_m@etna-alternance.net>
2023-01-19 23:59:27 +0100

    add: join event

--- Api/controllers/eventController.js
+++ Api/controllers/eventController.js
@@ -2,9 +2,8 @@ const Event = require("../models/eventsModel");
 
 const createEvent = async (req, res) => {
     try {
-        
         const newEvent = new Event(req.body);
-        
+
         await newEvent.save();
         return res.status(201).json({
             message: "New event successfully created ! 🔥",
@@ -17,12 +16,29 @@ const createEvent = async (req, res) => {
 
 const joinEvent = async (req, res) => {
     try {
-        const {eventId} = req.params;
-
-        
+        const { eventId } = req.params;
+        const { userId } = req.body;
+        const event = await Event.findById(eventId);
+        if (!event) {
+            return res.status(404).json({
+                message: "Event not found",
+            });
+        }
+        const user = await Users.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+        event.participants.push(user);
+        await event.save();
+        return res.status(200).json({
+            message: "User successfully joined the event",
+            event,
+        });
     } catch (error) {
-        
+        return res.status(500).json(error.message);
     }
-}
+};
 
-module.exports = { createEvent };
+module.exports = { createEvent, joinEvent };
--- Api/routes/eventRoute.js
+++ Api/routes/eventRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createEvent } = require("../controllers/eventController");
+const { createEvent, joinEvent } = require("../controllers/eventController");
 
 const eventRouter = express.Router();
 
 eventRouter.post("/event", createEvent);
+eventRouter.post("/event/:eventId/join", joinEvent);
 
 module.exports = eventRouter;

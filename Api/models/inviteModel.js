bf2baebbf03f48a860233f9ccedf1e0cd67f8692
MaloLelandais <leland_m@etna-alternance.net>
2023-02-20 12:06:12 +0100

    add: invite model

---
+++ Api/models/inviteModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const inviteSchema = new Schema({
+    type: {
+        required: true,
+        type: String,
+    },
+    eventId: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: "Event",
+    },
+    sender: {
+        required: true,
+        type: String,
+    },
+    receiver: {
+        required: true,
+        type: String,
+    },
+    date: {
+        required: true,
+        type: Date,
+        default: Date.now(),
+    },
+    status: {
+        required: true,
+        type: String,
+        enum: ["pending", "accepted", "rejected", "canceled"],
+    },
+});
+
+const Invites = mongoose.model("Invite", inviteSchema);
+module.exports = Invites;

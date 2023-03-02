8a9f23965714922021eb6e34ac4a5d361a8be777
Jean-Antoine <pondav_j@etna-alternance.net>
2023-03-02 11:18:53 +0100

    fix: problems wit login view

--- Front/src/pages/App/ProfilePage.js
+++ Front/src/pages/App/ProfilePage.js
@@ -1,10 +1,10 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import styled from "styled-components";
 
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "../../store/userSlice";
+
 import axios from "axios";
 import { useState } from "react";
 import { colors } from "../../../assets/colors";
@@ -15,14 +15,16 @@ const MainView = styled.View`
     width: 100%;
     padding: 20px;
 
+    align-items : center;
     border: 1px solid red;
     display: flex;
 `;
 
+
 const styles = StyleSheet.create({
     MainView: {
         width: "100%",
-        paddingVertical: "10%",
+        paddingVertical: "50%",
         backgroundColor: colors.blue,
     }
 })
@@ -47,27 +49,91 @@ align-items: center;
 height: 20%;
 `;
 
-const ProfilePage = ({ props }) => {
+
+
+const ProfilePage = ({  }) => {
+    const [userData, setUserData] = useState(null);
+    const [editedData, setEditedData] = useState({
+        email: "",
+        username: "",
+        password: "",
+      });
       
-   /* const userId = useSelector((state) => state.user.userInfo.userId);*/
-    return (
-        <ProfileView>
+      const handleInputChange = (field, value) => {
+        setEditedData({ ...editedData, [field]: value });
+      };
+      
+      const handleSubmit = async () => {
+        try {
+          const response = await axios.put(`/user/${userId}`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(editedData),
+          });
+          const data = await response.json();
+          setUserData(data.updated);
+          
+        } catch (error) {
+          console.log(error);
+        }
+      };
 
-            <TouchableOpacity>
-            <View>
-            
 
-                <Text>Profile Page</Text>
+    useEffect(() => {
+        const fetchUserData = async () => {
+          try {
+            const response = await axios.get(`/user/${userId}`);
+            const data = await response.json();
+            setUserData(data.user);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+      
+        fetchUserData();
+      }, []);
+   
+   
+   
+   return (
+    <MainView>
+        <View>
+        <Text>Email: {userData.email}</Text>
+        <Text>Username: {userData.username}</Text>
+        <Text>Phone Number: {userData.number}</Text>
+        <TouchableOpacity onPress={() => setIsEditing(true)}>
+        <Text>Edit</Text>
+        </TouchableOpacity>
+   
+      
 
-                <EditUserBtn onPress={() => null}>
-                        Edit
-                </EditUserBtn>
-                
-                
-            </View></TouchableOpacity>
-           
-        </ProfileView>
-    );
+      </View>
+      <TextInput
+        value={editedData.email}
+        onChangeText={(value) => handleInputChange("email", value)}
+        placeholder="Email"
+      />
+      <TextInput
+        value={editedData.username}
+        onChangeText={(value) => handleInputChange("username", value)}
+        placeholder="Username"
+      />
+      <TextInput
+        value={editedData.number}
+        onChangeText={(value) => handleInputChange("number", value)}
+        placeholder="Phone Number"
+        />
+      <TextInput
+        value={editedData.password}
+        onChangeText={(value) => handleInputChange("password", value)}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <Button title="Save" onPress={handleSubmit} />
+    </MainView>
+  );
 };
 
 export default ProfilePage;

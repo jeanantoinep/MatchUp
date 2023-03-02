f0d913686c72cd30f8a84ed06edf721ce94bb16f
Cedric Chau <cedricchau87@gmail.com>
2023-03-02 15:06:47 +0100

    fix: edit profile
    Co-authored-by: Jean-Antoine Pondaven <jeanantoinep@users.noreply.github.com>

--- Front/src/pages/App/ProfilePage.js
+++ Front/src/pages/App/ProfilePage.js
@@ -1,141 +1,138 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import styled from "styled-components";
+import profile from "../../../assets/profile.png";
 
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import axios from "axios";
 import { useState } from "react";
 import { colors } from "../../../assets/colors";
-
-
+import CustomInput from "../../components/CustomInput";
 
 const MainView = styled.View`
     width: 100%;
     padding: 20px;
-
-    align-items : center;
+    margin-top: 15%;
+    align-items: center;
     border: 1px solid red;
     display: flex;
 `;
-
 
 const styles = StyleSheet.create({
     MainView: {
         width: "100%",
         paddingVertical: "50%",
         backgroundColor: colors.blue,
-    }
-})
-
-
+    },
+});
 
 const Title = styled.Text`
     font-size: 28px;
     text-align: center;
 `;
-const EditUserBtn  = styled.Text`
-color: ${colors.red};
-font-size: 12px;
-text-align: center;
-text-decoration: underline;
+const EditUserBtn = styled.Text`
+    color: ${colors.red};
+    font-size: 12px;
+    text-align: center;
+    text-decoration: underline;
+`;
+
+const SubmitBtn = styled.TouchableOpacity`
+    background-color: ${colors.red};
+    padding: 0 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    margin: 30px 0;
 `;
 
 const ProfileView = styled.View`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 20%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20%;
 `;
 
-
-
-const ProfilePage = ({  }) => {
+const ProfilePage = ({}) => {
     const [userData, setUserData] = useState(null);
-    const [editedData, setEditedData] = useState({
-        email: "",
-        username: "",
-        password: "",
-      });
-      
-      const handleInputChange = (field, value) => {
-        setEditedData({ ...editedData, [field]: value });
-      };
-      
-      const handleSubmit = async () => {
-        try {
-          const response = await axios.put(`/user/${userId}`, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(editedData),
-          });
-          const data = await response.json();
-          setUserData(data.updated);
-          
-        } catch (error) {
-          console.log(error);
-        }
-      };
+    const [isLoading, setIsLoading] = useState(true);
 
+    const handleInputChange = (field, value) => {
+        setUserData({ ...userData, [field]: value });
+    };
+
+    const userId = useSelector((state) => state.user.userInfo.userId);
+
+    const handleSubmit = async () => {
+        try {
+            const { data } = await axios.put(`/user/${userId}`, userData);
+            // setUserData(data.updated);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     useEffect(() => {
         const fetchUserData = async () => {
-          try {
-            const response = await axios.get(`/user/${userId}`);
-            const data = await response.json();
-            setUserData(data.user);
-          } catch (error) {
-            console.log(error);
-          }
+            try {
+                const response = await axios.get(`/user/${userId}`);
+                setUserData(response.data.user);
+                setIsLoading(false);
+            } catch (error) {
+                console.log(error);
+            }
         };
-      
-        fetchUserData();
-      }, []);
-   
-   
-   
-   return (
-    <MainView>
-        <View>
-        <Text>Email: {userData.email}</Text>
-        <Text>Username: {userData.username}</Text>
-        <Text>Phone Number: {userData.number}</Text>
-        <TouchableOpacity onPress={() => setIsEditing(true)}>
-        <Text>Edit</Text>
-        </TouchableOpacity>
-   
-      
 
-      </View>
-      <TextInput
-        value={editedData.email}
-        onChangeText={(value) => handleInputChange("email", value)}
-        placeholder="Email"
-      />
-      <TextInput
-        value={editedData.username}
-        onChangeText={(value) => handleInputChange("username", value)}
-        placeholder="Username"
-      />
-      <TextInput
-        value={editedData.number}
-        onChangeText={(value) => handleInputChange("number", value)}
-        placeholder="Phone Number"
-        />
-      <TextInput
-        value={editedData.password}
-        onChangeText={(value) => handleInputChange("password", value)}
-        placeholder="Password"
-        secureTextEntry={true}
-      />
-      <Button title="Save" onPress={handleSubmit} />
-    </MainView>
-  );
+        fetchUserData();
+    }, []);
+
+    // useEffect(() => {
+    //     console.log("userDATA", userData);
+    // }, [userData]);
+
+    return isLoading ? (
+        <View>
+            <Text>loading</Text>
+        </View>
+    ) : (
+        <MainView>
+            <View></View>
+
+            <CustomInput
+                value={userData.firstname}
+                onChangeText={(value) => handleInputChange("firstname", value)}
+                name="Firstname"
+            />
+            <CustomInput
+                value={userData.email}
+                onChangeText={(value) => handleInputChange("email", value)}
+                name="Email"
+            />
+            <CustomInput
+                value={userData.username}
+                onChangeText={(value) => handleInputChange("username", value)}
+                name="Username"
+            />
+            <CustomInput
+                value={userData.number}
+                onChangeText={(value) => handleInputChange("number", value)}
+                name="Phone Number"
+            />
+            <SubmitBtn onPress={handleSubmit}>
+                <Text>Submit</Text>
+            </SubmitBtn>
+        </MainView>
+    );
 };
 
 export default ProfilePage;
-
-

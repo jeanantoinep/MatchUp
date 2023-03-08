3ebfd51772d12e90adeb8e9bb103003c88879754
Cedric Chau <cedricchau87@gmail.com>
2023-03-08 12:52:45 +0100

    add: style and other fix

--- Front/src/components/EventDetails.js
+++ Front/src/components/EventDetails.js
@@ -8,6 +8,9 @@ import {
 import React from "react";
 import styled from "styled-components";
 import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+
 import displayDate from "../utils/displayDate";
 import displayTime from "../utils/displayTime";
 import { useNavigation } from "@react-navigation/native";
@@ -15,13 +18,15 @@ import { useNavigation } from "@react-navigation/native";
 const Title = styled.Text`
     font-size: 28px;
     text-align: center;
-    margin: 16px 0;
+    margin: 10px 0;
 `;
 
 const LineView = styled.View`
     display: flex;
     flex-direction: row;
+    justify-content: flex-start;
     align-items: center;
+    width: 80%;
 `;
 
 const IconView = styled.View`
@@ -31,34 +36,75 @@ const IconView = styled.View`
     align-items: center;
 `;
 
+const LineText = styled.Text`
+    width: 80%;
+`;
+
+const ListHeaderView = styled.View`
+    margin: 20px 0 10px 0;
+`;
+
+const ListHeaderText = styled.Text`
+    font-size: 18px;
+`;
+
+const DescriptionView = styled.View`
+    margin: 16px 0;
+`;
+
+const DescriptionText = styled.Text``;
+
 const EventDetails = ({ event }) => {
     return (
-        <View>
-            <Title>{event.name}</Title>
-            <LineView>
-                <IconView>
-                    <AntDesign name="user" size={24} color="black" />
-                </IconView>
-                <Text>
-                    Looking for :{" "}
-                    {event.nb_participants - event.participants.length - 1}{" "}
-                    player(s)
-                </Text>
-            </LineView>
-            <LineView>
-                <IconView>
-                    <AntDesign name="calendar" size={24} color="black" />
-                </IconView>
+        <>
+            <View>
+                <Title>{event.name}</Title>
 
-                <Text>
-                    {displayDate(new Date(event.startDate))},{" "}
-                    {displayTime(
-                        new Date(event.startDate),
-                        new Date(event.endDate)
-                    )}
-                </Text>
-            </LineView>
-        </View>
+                <LineView>
+                    <IconView>
+                        <AntDesign name="user" size={24} color="black" />
+                    </IconView>
+                    <LineText>{event.creator.username}</LineText>
+                </LineView>
+                <LineView>
+                    <IconView>
+                        <FontAwesome5 name="users" size={20} color="black" />
+                    </IconView>
+                    <LineText>
+                        Looking for :{" "}
+                        {event.nb_participants - event.participants.length - 1}{" "}
+                        player(s)
+                    </LineText>
+                </LineView>
+                <LineView>
+                    <IconView>
+                        <Entypo name="location-pin" size={24} color="black" />
+                    </IconView>
+                    <LineText>{event.location}</LineText>
+                </LineView>
+                <LineView>
+                    <IconView>
+                        <AntDesign name="calendar" size={24} color="black" />
+                    </IconView>
+
+                    <LineText>
+                        {displayDate(new Date(event.startDate))},{" "}
+                        {displayTime(
+                            new Date(event.startDate),
+                            new Date(event.endDate)
+                        )}
+                    </LineText>
+                </LineView>
+                {event.description && (
+                    <DescriptionView>
+                        <DescriptionText>{event.description}</DescriptionText>
+                    </DescriptionView>
+                )}
+            </View>
+            <ListHeaderView>
+                <ListHeaderText>Participants : </ListHeaderText>
+            </ListHeaderView>
+        </>
     );
 };
 
